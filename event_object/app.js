7ea7ff96a5b8b81a1c011374b85827d5e8d0a9e9
gauravgdeshpande <gdgd3607@gmail.com>
2016-06-02 14:29:41 +0530

    mouse_coordinates_example

---
+++ event_object/app.js
@@ -0,0 +1,8 @@
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.myFunc = function(myE) {
+        $scope.x = myE.clientX;
+        $scope.y = myE.clientY;
+    }
+});
